b4c89215ad03b3c82ff49db9990f7d5f013b9a47
Taerghods <sanazzt88@gmail.com>
2025-07-11 14:01:52 +0330

    edit sth

--- src/urls.py
+++ src/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls'), name='products'),
-    # path('', include('accounts.urls'), name='accounts'),
+    # path('main/', include('accounts.urls'), name='accounts'),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
